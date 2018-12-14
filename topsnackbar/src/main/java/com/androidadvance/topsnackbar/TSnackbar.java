7ad4b7d553b70c5166cf7cb499de873755be5e17
muthu prakash <muthu.prakash3gmail.com>
2018-12-14 17:28:07 +0530

    Fixed an issue when an empty layout is shown when trying to show the snackbar after the first time. Each time a new Instance of snackbar is used to show instead of using the old snack bar instance this fix will allow to use the same instance of the snackbar

--- topsnackbar/src/main/java/com/androidadvance/topsnackbar/TSnackbar.java
+++ topsnackbar/src/main/java/com/androidadvance/topsnackbar/TSnackbar.java
@@ -742,7 +742,7 @@ public final class TSnackbar {
         @Override
         protected void onLayout(boolean changed, int l, int t, int r, int b) {
             super.onLayout(changed, l, t, r, b);
-            if (changed && mOnLayoutChangeListener != null) {
+            if (mOnLayoutChangeListener != null) {
                 mOnLayoutChangeListener.onLayoutChange(this, l, t, r, b);
             }
         }
